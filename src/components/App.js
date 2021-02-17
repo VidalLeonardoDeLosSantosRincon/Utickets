8cbdb6e3fefa802c24e4d1c91bfffdcc9bdcf3a3
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-16 22:10:46 -0400

    Header, footer and content were added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
-function App() {
+//components
+import {Header} from "./global/Header";
+import {Footer} from "./global/Footer";
+import {Content} from "./global/Content";
+
+function App({children}) {
   return (
     <div className="App">
- 
+        <Header/>
+        <Content>
+          {children}
+        </Content>
+        <Footer/>
     </div>
   );
 }
-
 export default App;
