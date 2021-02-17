fcf64599da180f5252c321c5bffa63bc285a6e42
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-16 21:48:33 -0400

    Homepage created

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from "react";
+
+export const Home  = (props) =>{
+    return (
+        <Fragment>
+            <div className="ctr-home">
+                Home page
+            </div>
+        </Fragment>
+    );
+}
