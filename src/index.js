d8aa53685d9f6729f19828dcb35079bcecb251c5
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-18 23:43:03 -0400

    provider and store were added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {AppRoutes} from './routes';
+import {Provider} from "react-redux";
+import {Store} from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRoutes />
+    <Provider store={Store}>
+      <AppRoutes/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
