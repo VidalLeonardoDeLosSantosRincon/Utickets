059604df19502027e6cec024d0c7c86a61913f1b
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-19 18:22:24 -0400

    ticket's model added

--- src/redux/reducers/tickets.js
+++ src/redux/reducers/tickets.js
@@ -1,3 +1,13 @@
+const Ticket = function(name, owner, date_created, priority, description = ""){
+    this.name = name; //required
+    this.owner = owner; //required
+    this.date_created = date_created; //required
+    this.date_clode = ""; 
+    this.time_opened = "";
+    this.priority = priority; //required
+    this.description = description;
+} 
+
 const INITIAL_STATE = {
     tickets_opened: [],
     tickets_for_review: [],
