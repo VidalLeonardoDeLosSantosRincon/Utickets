7b4f4240c09a5640d1c338b08782d28405cb29e7
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-19 17:35:48 -0400

    Array tickets_for_review added

--- src/redux/reducers/tickets.js
+++ src/redux/reducers/tickets.js
@@ -1,8 +1,9 @@
 const INITIAL_STATE = {
     tickets_opened: [],
+    tickets_for_review: [],
     tickets_closed: []
 };
 
-export const Tickets = (state= INITIAL_STATE, action)=>{
+export const Tickets = (state = INITIAL_STATE, action)=>{
 
 }
