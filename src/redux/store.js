6dc5dbcd1b9923fe696a9d86a68de1ad8ac58ccf
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-19 00:01:44 -0400

    ticket's reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import {createStore, combineReducers} from "redux";
 
 //reducers
+import {Tickets} from "./reducers/tickets";
 
-const reducers = combineReducers({});
+const reducers = combineReducers({Tickets});
 
 export const Store = createStore(reducers);
