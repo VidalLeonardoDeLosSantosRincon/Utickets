4246691256803aa125a98779b01465c8b9abc961
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-24 21:32:28 -0400

    component TicketsSection created and modified

---
+++ src/components/TicketsSection/index.js
@@ -0,0 +1,46 @@
+import React, {Fragment} from "react";
+
+//components
+import {TicketBox} from "../TicketBox";
+
+export const TicketsSection = () =>{
+    return (
+        <Fragment>
+
+            <div className="ctr-tickets">
+                <div className="ctr-tickets_section">
+                    <TicketBox/>
+                    <TicketBox/>
+                    <TicketBox/>
+                    <TicketBox/>
+                    <TicketBox/>
+                </div>
+            </div>
+            <style jsx="true">
+            {`
+
+                .ctr-tickets{
+                    margin-top:20px;
+                    padding:5px;
+                    box-sizing:border-box;
+                    background:white;
+                    height:100vh;
+                }
+                .ctr-tickets_section{
+                    height:auto;
+                    width:100%;
+                    padding:5px;
+                    box-sizing:border-box;
+                    background:rgba(0,0,0,0.1);
+                    margin:0 auto;
+                    overflow-x:scroll;
+        
+                    display:flex;
+                    justify-content:flex-start;
+                    align-items:center;
+                }
+            `}
+            </style>
+        </Fragment>
+    ); 
+}
