2615fa40f39096932051f2381bdbf3448904f23a
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-25 23:55:52 -0400

    Component Footer was modified

--- src/components/global/Footer/index.js
+++ src/components/global/Footer/index.js
@@ -5,7 +5,7 @@ export const Footer = ()=>{
         <Fragment>
             <div className="ctr-footer">
                 <footer>
-                    footer
+                    <h4 className="title"><span>Vidal De Los</span> Santos 2021</h4>
                 </footer>
             </div>
             <style jsx="true">
@@ -21,11 +21,28 @@ export const Footer = ()=>{
 
                 .ctr-footer footer{
                     width:100%;
-                    height:150px;
-                    background:gray;
+                    height:100px;
+                    background:black;
                     color:white;
                     padding:10px 25px;
                     box-sizing:border-box;
+
+                    display:flex;
+                    justify-content:center;
+                    align-items:flex-start;
+                    
+                }
+
+                .ctr-footer footer .title{
+                    color:white;
+                    font-size:18px;
+                    font-weight:bold;
+                }
+
+                .ctr-footer footer .title span{
+                    color:lightseagreen;
+                    font-size:18px;
+                    font-weight:bold;
                 }
             `}
             </style>
