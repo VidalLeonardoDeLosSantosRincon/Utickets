baee5e0a1e4d8348280a526b85bc809f18a27469
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-27 23:30:30 -0400

    Component SocialNetworks was created

---
+++ src/components/global/SocialNetworks/index.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from "react";
+
+
+export const SocialNetWork = ()  =>{
+    return (
+        <Fragment>
+            <div className="ctr-social_network">
+                socialnetwoks
+            </div>
+        </Fragment>
+    );
+}
