34ca7954df521dc307806f10b20994364b6d1766
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-02-28 23:01:35 -0400

    list added

--- src/components/global/SocialNetworks/index.js
+++ src/components/global/SocialNetworks/index.js
@@ -1,12 +1,45 @@
 import React, {Fragment} from "react";
 
 
-export const SocialNetWork = ()  =>{
+export const SocialNetwork = ()  =>{
     return (
         <Fragment>
             <div className="ctr-social_network">
-                socialnetwoks
+                <ul className="menu-social_network">
+                    <li>Linkedin</li>
+                    <li>YouTube</li>
+                    <li>Github</li>
+                </ul>    
             </div>
+            <style jsx="true">
+                {`
+                    .ctr-social_network{
+                        padding: 5px;
+                        box-sizing: border-box;
+                        background: black;
+                       
+                    }
+                    
+                    .ctr-social_network .menu-social_network{
+                        list-style:none;
+                        padding:5px;
+                        box-sizing:border-box;
+                        background:black;
+                        width:350px;
+
+                        display:flex;
+                        justify-content:space-around;
+                        align-items:center;
+                    }
+
+                    .ctr-social_network .menu-social_network li{
+                        display: inline-block;
+                        padding:5px 10px;
+                        box-sizing:border-box;
+                        background:black;
+                    }
+                `}
+            </style>
         </Fragment>
     );
 }
