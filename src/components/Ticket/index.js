2822706d18979f3a21c5ae0a33cdbecee739eedc
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2021-03-15 21:39:46 -0400

    adding hover effect in ticket box

--- src/components/Ticket/index.js
+++ src/components/Ticket/index.js
@@ -4,7 +4,7 @@ export const Ticket = ()=>{
     return(
         <Fragment>
             <div className="ctr-ticket">
-                aaaaaaaaaaaaa
+                <p>Description</p>
             </div>
             <style jsx="true" global="false">
             {`
@@ -16,10 +16,25 @@ export const Ticket = ()=>{
                     box-sizing:border-box;
                     background:white;
                     width:100%;
-                    border:0.5px solid rgba(0, 0, 0, 0.05);
+                    border:0.5px solid rgba(0, 0, 0, 0.2);
                     border-radius:5px;
                     box-shadow:2px 2px 5px rgba(0, 0, 0, 0.2);
                     cursor:pointer;
+
+                    display:flex;
+                    justify-content:center;
+                    align-items:center;
+                }
+
+                .ctr-ticket:hover{
+                    transform:scale(1.02);
+                    transition:all 0.1s linear;
+                    box-shadow:2px 4px 6px rgba(0, 0, 0, 0.2);
+                }
+
+                .ctr-ticket p {
+                    font-weight:300px;
+                    font-size:12px;
                 }
             `}
             </style>
